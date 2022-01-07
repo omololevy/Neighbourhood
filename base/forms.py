eb0f8ca65034ed6a4e6e82ae7d20577984c5e3a0
Levy Omolo <levy.omolo@student.moringaschool.com>
2022-01-07 18:27:50 +0300

    Create the forms file

---
+++ base/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UsernameField
+from .models import NeighbourHood,Business,Post,Profile
+
+User = get_user_model()
+
+class NeighbourHoodCreateForm(forms.ModelForm):
+    hood_image = forms.FileField(),
+    class Meta:
+        model= NeighbourHood
+        fields = (
+            'Neighbourhood_name',
+            'Neighbourhood_location',
+            'Neighbourhood_image',
+            'about_hood',
+            'occupants_count',
+            'health_info',
+            'police_info'
+            
+        )
+        
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model=Business
+        fields = (
+            'business_name',
+            'business_email',
+            'about_business' 
+        )
+class PostForm(forms.ModelForm):
+    class Meta:
+        model=Post
+        fields = (
+            'title',
+            'post',
+        )
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(max_length=254)
+    fullname=forms.CharField(max_length=254)
+    
+    class Meta:
+        model = User
+        fields = ("username",'fullname', 'email',)
+        
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('bio', 'profile_picture')

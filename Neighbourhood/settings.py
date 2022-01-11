26efea6101909fa05ee8a32225a142f027bcf78d
Levy Omolo <levy.omolo@student.moringaschool.com>
2022-01-12 00:03:30 +0300

    Delete the Csv importation

--- Neighbourhood/settings.py
+++ Neighbourhood/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import dj_database_url
-from decouple import config, Csv
+from decouple import config
 import cloudinary
 import django_on_heroku
 

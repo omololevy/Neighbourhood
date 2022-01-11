16a9a78c3b220016da6282f007f756b5811f3541
Levy Omolo <levy.omolo@student.moringaschool.com>
2022-01-11 19:13:31 +0300

    Add the Model classes in the admin file

---
+++ base/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Profile, NeighbourHood, Business, Post
+
+
+admin.site.register(Profile)
+admin.site.register(NeighbourHood)
+admin.site.register(Business)
+admin.site.register(Post)

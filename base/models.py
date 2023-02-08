66708fb76a1005ba3d562d6900c022f8b55b8afe
Levy Omolo <levy.omolo@student.moringaschool.com>
2023-02-08 04:00:49 +0300

    Update models.py

--- base/models.py
+++ base/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-from pyuploadcare.dj.models import ImageField
+# from pyuploadcare.dj.models import ImageField
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
